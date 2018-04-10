c989c97bc1b0a83646fecdd4798f0f2ee182ea38
楊育宗 <b96902108@gmail.com>
2018-04-10 15:58:45 +0100

    first commit

---
+++ Handwriting/CategoryCell.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryCell.swift
+//  Handwriting
+//
+//  Created by 楊育宗 on 2018/4/10.
+//  Copyright © 2018年 楊育宗. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var coverImageView: UIImageView!
+}
--- Handwriting/ViewController.swift
+++ Handwriting/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
+struct Category {
+    var categoryName: String
+    var categoryImage: UIImage?
+}
+
 class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    var categoryArray = [Category]()
+    var fullScreenSize :CGSize!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.title = "My Handwriting"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addCategoryAction(sender:)))
+
+        fullScreenSize = UIScreen.main.bounds.size
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addCategoryAction(sender: Any) {
+        let alertController = UIAlertController(title: "New Category", message: "Please enter new category name", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "My Handwriting"
+        }
+        let saveAction = UIAlertAction(title: "Save", style: UIAlertActionStyle.default, handler: { alert -> Void in
+            let textField = alertController.textFields![0] as UITextField
+            if textField.text!.count > 0 {
+                print("Input string is \(textField.text!)")
+                self.categoryArray.append(Category(categoryName: textField.text!, categoryImage: nil))
+                self.collectionView.reloadData()
+            }
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: {
+            (action : UIAlertAction!) -> Void in })
+
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+
+        self.present(alertController, animated: true, completion: nil)
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.categoryArray.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: CGFloat(fullScreenSize.width)/2 - 20.0, height: CGFloat(fullScreenSize.width)/2 - 20.0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: CategoryCell.self), for: indexPath) as! CategoryCell
+
+        let category = self.categoryArray[indexPath.row]
+        cell.nameLabel.text = category.categoryName
+
+        return cell
+    }
+}
