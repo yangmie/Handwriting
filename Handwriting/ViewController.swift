08ccb45cfc2d180f4f10e1fda3be121423d3f3b4
楊育宗 <b96902108@gmail.com>
2018-04-10 17:43:51 +0100

    save category ok

--- Handwriting/ViewController.swift
+++ Handwriting/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 struct Category {
     var categoryName: String
@@ -17,18 +18,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
 
+    var appDelegate: AppDelegate!
+    var container: NSPersistentContainer!
+    var context: NSManagedObjectContext!
+
     var categoryArray = [Category]()
     var fullScreenSize :CGSize!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        appDelegate = UIApplication.shared.delegate as! AppDelegate
+        container = appDelegate.persistentContainer
+        context = container.viewContext
 
-        self.title = "My Handwriting"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addCategoryAction(sender:)))
-        self.navigationController?.navigationBar.barTintColor = UIColor_Yang.init(rgb: 0x8fabd8, alpha: 1)
-        self.navigationController?.navigationBar.tintColor = UIColor.black
+        title = "My Handwriting"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addCategoryAction(sender:)))
+        navigationController?.navigationBar.barTintColor = UIColor_Yang.init(rgb: 0x8fabd8, alpha: 1)
+        navigationController?.navigationBar.tintColor = UIColor.black
         
         fullScreenSize = UIScreen.main.bounds.size
+        fetchCategories()
     }
 
     @IBAction func addCategoryAction(sender: Any) {
@@ -36,37 +45,65 @@ class ViewController: UIViewController {
         alertController.addTextField { (textField : UITextField!) -> Void in
             textField.placeholder = "My Handwriting"
         }
+        
         let saveAction = UIAlertAction(title: "Save", style: UIAlertActionStyle.default, handler: { alert -> Void in
             let textField = alertController.textFields![0] as UITextField
             if textField.text!.count > 0 {
-                print("Input string is \(textField.text!)")
-                self.categoryArray.append(Category(categoryName: textField.text!, categoryImage: nil))
-                self.collectionView.reloadData()
+                self.saveCategory(name: textField.text!)
+                self.fetchCategories()
             }
         })
+
         let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: {
             (action : UIAlertAction!) -> Void in })
 
         alertController.addAction(saveAction)
         alertController.addAction(cancelAction)
 
-        self.present(alertController, animated: true, completion: nil)
+        present(alertController, animated: true, completion: nil)
+    }
+
+    func fetchCategories() {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CategoryData")
+        do {
+            let categories = try context.fetch(fetchRequest)
+
+            categoryArray = []
+            for c in categories as! [NSManagedObject] {
+                categoryArray.append(Category(categoryName: c.value(forKey: "categoryName") as! String, categoryImage: nil))
+                collectionView.reloadData()
+            }
+        } catch {
+            print("entered catch for categories fetch request")
+        }
+    }
+
+    func saveCategory(name: String) {
+        let entity = NSEntityDescription.entity(forEntityName: "CategoryData", in: context)
+        let newCategory = NSManagedObject(entity: entity!, insertInto: context)
+        newCategory.setValue(name, forKey: "categoryName")
+
+        do {
+            try context.save()
+        } catch {
+            print("Failed saving")
+        }
     }
 }
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.categoryArray.count
+        return categoryArray.count
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: CGFloat(fullScreenSize.width)/2 - 20.0, height: CGFloat(fullScreenSize.width)/2 - 20.0)
+        return CGSize(width: CGFloat(fullScreenSize.width)/2 - 20.0, height: CGFloat(fullScreenSize.width)/2*1.1)
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: CategoryCell.self), for: indexPath) as! CategoryCell
 
-        let category = self.categoryArray[indexPath.row]
+        let category = categoryArray[indexPath.row]
         cell.nameLabel.text = category.categoryName
 
         return cell
